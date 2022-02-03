5cbe21faa9a3b994aff315597b8f021f0f759a4b
Sumitbansal-01 <83486679+Sumitbansal-01@users.noreply.github.com>
2022-02-03 21:33:10 +0530

    Update api.js

--- main/api.js
+++ main/api.js
@@ -8,16 +8,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/getcount',(req,res)=> {
-    res.send(fs.readFileSync('count.json','utf-8'))
+    res.send(fs.readFileSync('../count.json','utf-8'))
 })
 
 app.get('/get', (req, res) => {
-  res.send(fs.readFileSync('table.json','utf-8'))
+  res.send(fs.readFileSync('../table.json','utf-8'))
 })
 app.post("/post",(req,res) => {
     const id =req.body.id
     const desc=req.body.desc
-    let data=JSON.parse(fs.readFileSync('table.json','utf-8'))
+    let data=JSON.parse(fs.readFileSync('../table.json','utf-8'))
     if (req.body.update===undefined){
     data.push({'id':id,'desc':desc})
     }else if(req.body.update) {
@@ -28,18 +28,18 @@ app.post("/post",(req,res) => {
             return n
         })
     }
-    fs.readFile('count.json','utf-8',(err,data)=>{
+    fs.readFile('../count.json','utf-8',(err,data)=>{
         if(err){
             console.error(err)
         }else{
-            fs.writeFile('count.js',JSON.stringify(JSON.parse(Number(data)+1)),(err)=>{
+            fs.writeFile('../count.js',JSON.stringify(JSON.parse(Number(data)+1)),(err)=>{
                 if(err){
                     console.error(err)
                 }
             })
         }
     })
-    fs.writeFileSync('table.json',JSON.stringify(data))
+    fs.writeFileSync('../table.json',JSON.stringify(data))
     console.log(req.body,data);
     res.statusCode=200
     res.end('Completed')
