749ea157c8159a523626fd2d5855bfea2c61ee99
Sumitbansal-01 <83486679+Sumitbansal-01@users.noreply.github.com>
2022-02-03 22:01:48 +0530

    Update api.js

--- main/api.js
+++ main/api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs=require('fs')
 const bodyParser = require("body-parser")
 const app = express()
-const port =  process.env.PORT ||4000;
+const port =  4000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
